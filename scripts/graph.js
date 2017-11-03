b0d307accbdfd5ac693a7924bc1d1ae01dd8ad0f
MirjamFi <figasch@informatik.uni-tuebingen.de>
2017-11-03 17:29:59 +0100

    updated legend for booleans

--- scripts/graph.js
+++ scripts/graph.js
@@ -2,71 +2,91 @@
   create a graph with Cytoscape
 */
 
+var nodes, edges, path, tracer, nodeVal, outputName, nodeAttributes, graphString;
+var firstTime = true;
+var loadGraphCount = 0;
+var legendDrawn = false;
+var svg;
+var  nodesMin = -1;
+var nodesMax = 1;
 
 /* 
 initiate cytoscape graph 
 */
 var cy = cytoscape({
-  container: document.getElementById('cy'),
-  style: [
-       // style nodes
-    {selector: 'node',
-      style: {
-        width: 50,
-        height: 50,
-        shape: 'ellipse',
-        'background-color': 'white',
-        'border-color' : 'black',
-        'border-style' : 'solid',
-        'border-width' : '2',
-        label: 'data(symbol)',
-        "text-valign" : "center",
-        "text-halign" : "center",
-        "font-size" : 10
-    }},
-    {selector: 'node[val < 0]',
-      style: {
-        'background-color': 'mapData(val, -10, 0, blue, white)'
-    }},
-    {selector: 'node[val > 0]',
-      style: {
-        'background-color': 'mapData(val, 0, 10, white, red)'
-    }},
+    container: document.getElementById('cy'),
+    style: [
+         // style nodes
+      {selector: 'node',
+        style: {
+          width: 50,
+          height: 50,
+          shape: 'ellipse',
+          'background-color': 'white',
+          'border-color' : 'black',
+          'border-style' : 'solid',
+          'border-width' : '2',
+          label: 'data(symbol)',
+          "text-valign" : "center",
+          "text-halign" : "center",
+          "font-size" : 10
+      }},
+      // attributes with numbers
+      {selector: 'node[val <0]',
+        style: {
+          'background-color': 'mapData(val,'+ nodesMin+', 0, #006cf0, white)'
+      }},
+      {selector: 'node[val >0]',
+        style: {
+          'background-color': 'mapData(val, 0,'+ nodesMax+', white, #d50000)'
+      }},
+      {selector: 'node[val = 0]',
+        style: {
+          'background-color': 'white'
+      }},
 
-    // style edges
-    {selector: 'edge',
-      style: {
-        'target-arrow-shape': 'triangle',
-        'arrow-scale' : 2,
-        'curve-style' : 'bezier'
+      // attributes with boolean
+      {selector: 'node[val = "false"]',
+        style: {
+          'background-color': '#006cf0'
       }},
-    {selector: 'edge[interaction = \'compound\']',
-      style: {
-        'target-arrow-shape': 'triangle-backcurve',
+      {selector: 'node[val = "true"]',
+        style: {
+          'background-color': '#d50000'
       }},
-    {selector: 'edge[interaction = \'activaion\']',
-      style: {
-        'target-arrow-shape': 'triangle',
-      }},
-    {selector: 'edge[interaction = \'expression\']',
-      style: {
-        'target-arrow-shape': 'triangle-backcurve',
-      }},
-    {selector: 'edge[interaction = \'phosphorylation\']',
-      style: {
-        'target-arrow-shape': 'diamond',
-      }},
-    {selector: 'edge[interaction = \'inhibition\']',
-      style: {
-        'target-arrow-shape': 'tee',
-      }},
-  ]
-});
 
-var nodes, edges, path, tracer, nodeVal, outputName, nodeAttributes, graphString;
-var firstTime = true;
-var loadGraphCount = 0;
-var legendDrawn = false;
+      // style edges
+      {selector: 'edge',
+        style: {
+          'target-arrow-shape': 'triangle',
+          'arrow-scale' : 2,
+          'curve-style' : 'bezier'
+        }},
+      {selector: 'edge[interaction = \'compound\']',
+        style: {
+          'target-arrow-shape': 'triangle-backcurve',
+        }},
+      {selector: 'edge[interaction = \'activaion\']',
+        style: {
+          'target-arrow-shape': 'triangle',
+        }},
+      {selector: 'edge[interaction = \'expression\']',
+        style: {
+          'target-arrow-shape': 'triangle-backcurve',
+        }},
+      {selector: 'edge[interaction = \'phosphorylation\']',
+        style: {
+          'target-arrow-shape': 'diamond',
+        }},
+      {selector: 'edge[interaction = \'inhibition\']',
+        style: {
+          'target-arrow-shape': 'tee',
+        }}
+      ]
+  });
+
+
+
 
 // test if object is empty
 function isEmpty(obj) {
@@ -78,8 +98,7 @@ function isEmpty(obj) {
 }
 
 //remove all options of dropdown
-function removeOptions(selectbox)
-{
+function removeOptions(selectbox){
     var i;
     for(i = selectbox.options.length - 1 ; i >= 0 ; i--)
     {
@@ -90,7 +109,6 @@ function removeOptions(selectbox)
 // read from grphml - file
 function loadFile() {
   path = document.getElementById('graphName').value;
-  console.log(path.endsWith('.graphml'));
   if(!path.endsWith('.graphml')){
     alert('Please give a .graphml-file.');
     return;
@@ -130,10 +148,9 @@ function loadFile() {
 };
 
 function createLegend(){
-  document.getElementById('legend').setAttribute('style','visibility:visible');
 
   //Append a defs (for definition) element to your SVG
-  var svg = d3.select("#legend").append("svg");
+  svg = d3.select("#legend").append("svg");
   svg.attr("width", 189).attr("height", 220);
   var defs = svg.append("defs");
 
@@ -143,9 +160,9 @@ function createLegend(){
 
   linearGradient.selectAll("stop") 
     .data([                             
-        {offset: "0%", color: "#0000ff"}, 
+        {offset: "0%", color: "#006cf0"}, 
         {offset: "50%", color: "#ffffff"},          
-        {offset: "100%", color: "#ff0000"}    
+        {offset: "100%", color: "#d50000"}    
       ])                  
     .enter().append("stop")
     .attr("offset", function(d) { return d.offset; }) 
@@ -156,28 +173,34 @@ function createLegend(){
   .attr("height", 20)
   .style("fill", "url(#linear-gradient)");
 
-  svg.append("text")      // text label for the x axis
-        .attr("x", 10 )
-        .attr("y", 29 )
-        .style("text-anchor", "middle")
-        .text("-10");
-  svg.append("text")      
+  if(Number.isFinite(nodesMin)){
+    svg.append("text")      
       .attr("x", 94.5 )
       .attr("y", 29 )
       .style("text-anchor", "middle")
-      .text("0");
-  svg.append("text")      
+      .text("0")
+      .attr("id", "mid");
+  }
+
+  svg.append("text")      // text label for the x axis
+        .attr("x", 12 )
+        .attr("y", 29 )
+        .style("text-anchor", "middle")
+        .text(nodesMin)
+        .attr("id", 'min');
+
+  /*svg.append("text")      // legend title
       .attr("x", 94.5 )
       .attr("y", 40 )
       .style("text-anchor", "middle")
-      .text("log2ratio");
+      .text("")*/
   svg.append("text")      // text label for the x axis
-      .attr("x", 183 )
+      .attr("x", 179 )
       .attr("y", 29 )
       .style("text-anchor", "middle")
-      .text("10");
+      .text(nodesMax)
+      .attr("id",'max');
 
-  //document.getElementById("defs").setAttribute('style','visibility:visible');*/
 
   $('#visualize').attr("disabled", true);
 }
@@ -186,10 +209,11 @@ function createLegend(){
 function visualize() {
 
   nodeVal = document.getElementById('values').value;
-  
+
   // get nodes and edges
   nodes = [];
   edges = [];
+  nodeValuesNum = [];
 
   var regExp = /\>([^)]+)\</; // get symbol name between > <
 
@@ -204,8 +228,15 @@ function visualize() {
         curNode.symbol = symbol;
       }
       if(graphString[i].includes("\"v_"+nodeVal+"\"\>")){
-        var val = parseFloat(regExp.exec(graphString[i])[1]); // if availabe get node value
+        var val = regExp.exec(graphString[i])[1]; // if availabe get node value
+        if(!isNaN(parseFloat(val))){
+          val = parseFloat(val);
+          nodeValuesNum.push(val);
+        }
         curNode.val = val;
+      }
+      if(!curNode.val){
+        curNode.val = 0;
       }
       nodes.push({data: curNode}); 
     }
@@ -226,6 +257,42 @@ function visualize() {
       edges.push({data: curEdge} );
     }
   }
+
+  // if attributes values are only 0 or 1 make them boolean
+  if(!isEmpty(nodeValuesNum)){
+    uniqueVals = new Set(nodeValuesNum);
+    console.log(uniqueVals);
+    if(uniqueVals.size == 2){
+      nodes.forEach( function(nodeEntry){
+        if(nodeEntry.data.val == 0){
+           nodeEntry.data.val = "false";
+        }
+        if(nodeEntry.data.val == 1){
+           nodeEntry.data.val = "true";
+        }   
+      });
+      nodeValuesNum = ["empty"]
+    }
+  }
+
+  // set min and max for legend
+  if(!isEmpty(nodeValuesNum)){
+    if(!nodeValuesNum.includes("empty")){
+      nodesMin = Math.max.apply(Math,nodeValuesNum).toFixed(2);
+      nodesMax = Math.max.apply(Math,nodeValuesNum).toFixed(2);
+    }
+    if(nodesMin >= 0 ){
+      nodesMin = -1;
+    }
+    if(nodesMax <= 0){
+      nodesMax = 1;
+    }
+  }
+  else{
+    nodesMin = "false";
+    nodesMax = "true";
+  }
+
   // add nodes and edges to graph
   if(loadGraphCount > 1){
     cy.elements().remove();
@@ -243,18 +310,42 @@ function visualize() {
       });
     }
   }
-  // calculate layout only once
+
+  // calculate layout and legend only once
   if(firstTime){
       firstTime = false;
       if(!legendDrawn){
         legendDrawn = true;
-        createLegend();
       }
       cy.layout({
       name: 'cose',//'breadthfirst',
         }).run();
       $('#download').removeAttr('disabled');
+      createLegend();
+
   }
+
+  // show legend and update if necessary
+  document.getElementById('legend').setAttribute('style','visibility:visible');
+  if(Number.isFinite(nodesMin)){    // numerical attribute
+    $("#mid").text("0");
+    cy.style()                // update the elements in the graph with the new style
+      .selector('node[val <0]')
+          .style('background-color', 'mapData(val,'+ nodesMin+', 0, #006cf0, white)')
+    .update();
+    cy.style()
+      .selector('node[val >0]')
+        .style('background-color', 'mapData(val,0,'+ nodesMax+', white, #d50000)')
+      .update(); 
+  }
+  else{
+    $("#mid").text("");         // boolean attribute
+  }
+
+  $("#min").text(nodesMin);
+  $("#max").text(nodesMax);
+
+
 
 }
 
