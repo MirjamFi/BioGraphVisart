1611724a0614e228f496dc8c0b5ea3f30d84bcc1
MirjamFi <figasch@informatik.uni-tuebingen.de>
2018-01-26 10:47:04 +0100

    "Upload" file in any directory or select from list

---
+++ data/S05/rnaseq_abs_log2fold_gt2_OR_vcf_incoming_OR_outgoing/sorafenib_to_lxr/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+HCC_SUBGRAPHS="/share/projects/HCCMultiscale/subgraphs"
+BINDIR="$HCC_SUBGRAPHS/bin"
+
+BASE_PATIENT=$1
+declare -a ALL_PATIENTS=("S02" "S05" "S06" "S07" "S11")
+
+SCORES="$HCC_SUBGRAPHS/scores/${BASE_PATIENT}/${BASE_PATIENT}_rnaseq_vcf.csv"
+GRAPH="$HCC_SUBGRAPHS/graphs/kegg/kegg_all_interactions.graphml"
+TERMINALS="$HCC_SUBGRAPHS/layers/lxr_merge_entrez.txt"
+RECEPTORS="$HCC_SUBGRAPHS/layers/sorafenib_targets_entrez.txt"
+
+echo "Finding subgraphs..."
+
+avgdrgnt.py --scores ${SCORES} \
+	    --score-column rnaseq_abs_log2fold_gt2_OR_vcf_incoming_OR_outgoing \
+	    --sep comma \
+	    --receptors-file ${RECEPTORS} \
+	    --terminals-file ${TERMINALS} \
+	    --graph ${GRAPH} \
+
+echo "Mapping attributes..."	    
+	    
+$BINDIR/annotate_with_layers.py optimal.graphml
+$BINDIR/annotate_with_scores.py optimal.graphml  ${SCORES}
+$BINDIR/expand_kegg.py optimal.graphml
+
+echo "Mapping other patients..."
+
+mkdir -p mapping
+for p in "${ALL_PATIENTS[@]}"
+do
+    mkdir -p mapping/$p
+    SCORES="$HCC_SUBGRAPHS/scores/$p/${p}_rnaseq_vcf.csv"
+    $BINDIR/map_subgraph_to_other_patient.py optimal.graphml ${SCORES}
+    mv optimal_mapped.graphml mapping/$p
+done	
+
+rm -r mapping/$BASE_PATIENT
---
+++ data/S06/rnaseq_abs_log2fold_gt2_OR_vcf_incoming_OR_outgoing/sorafenib_target_as_root/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+HCC_SUBGRAPHS="/share/projects/HCCMultiscale/subgraphs"
+BINDIR="$HCC_SUBGRAPHS/bin"
+
+BASE_PATIENT=$1
+declare -a ALL_PATIENTS=("S02" "S05" "S06" "S07" "S11")
+
+SCORES="$HCC_SUBGRAPHS/scores/${BASE_PATIENT}/${BASE_PATIENT}_rnaseq_vcf.csv"
+GRAPH="$HCC_SUBGRAPHS/graphs/kegg/kegg_all_interactions.graphml"
+ROOT=$2
+
+echo "Finding subgraphs..."
+
+avgdrgnt.py --scores ${SCORES} \
+	    --score-column rnaseq_abs_log2fold_gt2_OR_vcf_incoming_OR_outgoing \
+	    --sep comma \
+	    --root ${ROOT} \
+	    --min-size 15 \
+	    --algorithm gcc \
+	    --graph ${GRAPH} 
+
+echo "Mapping attributes..."	    
+	    
+$BINDIR/annotate_with_layers.py optimal.graphml
+$BINDIR/annotate_with_scores.py optimal.graphml ${SCORES}
+$BINDIR/expand_kegg.py optimal.graphml
+
+echo "Mapping other patients..."
+
+mkdir -p mapping
+for p in "${ALL_PATIENTS[@]}"
+do
+    mkdir -p mapping/$p
+    SCORES="$HCC_SUBGRAPHS/scores/$p/${p}_rnaseq_vcf.csv"
+    $BINDIR/map_subgraph_to_other_patient.py optimal.graphml ${SCORES}
+    mv optimal_mapped.graphml mapping/$p
+done
+
+rm -r mapping/$BASE_PATIENT
+
+SYMBOL=$(hgncmapper -f entrez $ROOT)
+mkdir -p $SYMBOL
+mv optimal.graphml $SYMBOL
+mv mapping $SYMBOL
