497b69663bdb905b0bbd6609ab9c1854cb7b5b3b
MirjamFi <figasch@informatik.uni-tuebingen.de>
2019-03-12 14:20:53 +0100

    clean up, added comments

--- static/scripts/initialize.js
+++ static/scripts/initialize.js
@@ -16,6 +16,8 @@ var noAttr = false;
 read from grphml - file and initialize cy-object
 */
 function readFile() {
+
+  // if it is not the first graph read, delete all selectable options
   var myNode = document.getElementById("dataPart");
   var domValues = document.getElementById("values");
   if(domValues){  
@@ -30,7 +32,9 @@ function readFile() {
   if(domResetLayout)
     {domResetLayout.parentNode.removeChild(domResetLayout);}
 
+  // does no have attribute for coloring/shape?
   noAttr = false;
+
   if(shapeNode){
     shapeNode.elements().remove();
   }
@@ -39,6 +43,7 @@ function readFile() {
   nodesMax = 1;
   firstTime = true;
 
+    // no file selected by user
     var files = document.getElementById('graphName').files;
     if (!files.length) {
       alert('Please select a file!');
@@ -47,10 +52,13 @@ function readFile() {
 
     var file = files[0];
 
+    // IS file a graphml?
     if(!file["name"].endsWith("graphml")){
       alert('Please select a .graphml-file.');
       return;
     }
+
+    // read file
     path = file.name;
     var reader = new FileReader();
     reader.onloadend = function(evt) {
@@ -72,29 +80,27 @@ function loadFile() {
   drp.id = "values";
   drp.name = "values";
   drp.onchange = visualize;
-  // node attributes
-  document.getElementById("dataPart").appendChild(drp);
   drp.style.visibility = "visible";
-
-
+  document.getElementById("dataPart").appendChild(drp);
+  // node attributes
   var sele = document.createElement("OPTION");
   sele.value =  "";
   sele.text = "Choose node's attribute";
   drp.add(sele);
 
-
+  // attributes for changing node shape in dropdown
   var drpShapes = document.createElement("select");
   drpShapes.id = "nodeShapesAttr";
   drpShapes.name = "nodeShapesAttr";
   document.getElementById("dataPart").appendChild(drpShapes);
   drpShapes.style.visibility = "hidden";
 
-  var sele = document.createElement("OPTION");    
-  sele.text = "Choose node's attribute";
-  sele.value = "";
-  drpShapes.add(sele);
+  var seleShapeAttr = document.createElement("OPTION");    
+  seleShapeAttr.text = "Choose node's attribute";
+  seleShapeAttr.value = "";
+  drpShapes.add(seleShapeAttr);
 
- // shapes
+ // shapes dropdown
   var drpShape = document.createElement("select");
   drpShape.id = "nodeShapes";
   drpShape.name = "nodeShapes";
@@ -117,8 +123,11 @@ function loadFile() {
     drpShape.add(optnShape);
   })
 
+  // no attributes for node coloring/shape
   var noOptn = true;
   var noDrpShapes = true;
+
+  // get attributes for coloring -> double/boolean and shape -> boolean
   for (var i = 0; i <= graphString.length - 1; i++) {
     if(graphString[i].includes("for=\"node\"") && 
       (graphString[i].includes("attr.type=\"double\"") || 
@@ -139,10 +148,13 @@ function loadFile() {
         noDrpShapes = false;
       }
     };
+    // do not search whole file, only header
     if(graphString[i].includes("<node id=\"n0\">")){
       break;
     };
   };
+
+  // create button to reset layout
   var resetButton = document.createElement("button");
   resetButton.id = "resetLayout";
   var t = document.createTextNode("Reset layout");
@@ -152,6 +164,7 @@ function loadFile() {
   resetButton.style.visibility = "hidden";
   document.getElementById("dataPart").appendChild(resetButton);
 
+  // if no attributes found for coloring/shape, remove dropdown menus and visualize
   if(noOptn && noDrpShapes){
     noAttr = true;
     drp.parentNode.removeChild(drp);
