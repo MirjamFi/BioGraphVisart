b5397e293ce0d45abd76f3b0b78c707f59ca1fdf
MirjamFi <figasch@informatik.uni-tuebingen.de>
2019-10-22 13:26:07 +0200

    Create server.js

--- static/scripts/server.js
+++ static/scripts/server.js
@@ -32,45 +32,10 @@ var data;
 app.post('/vis', function(req, res) {
         data = req.body;
         res.end("done")
-    });
+});
 app.get('/vis', function(req, res) {
         res.send(data)
-    });
-
-
-
-
-app.get("/foundGraphs", function(req, res) {
-    if(foundFiles.length == 0){
-        var startPath = '../graphs/';
-        var filter = '.graphml';
-        fromDir(startPath, filter, foundFiles);
-    }   
-    res.send(foundFiles);}
-)
-
-
-function fromDir(startPath, filter){
-    if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
-        return;
-    }
-
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=path.join(startPath,files[i]);
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory()){
-            fromDir(filename,filter); //recurse
-        }
-        else if (filename.indexOf(filter)>=0) {
-            console.log('-- found: ',filename);
-            foundFiles.push(filename);
-        };
-    };
-    return foundFiles
-}
-
+});
 
 app.listen(3000);
 
