1c3797ab1af1c3188f1e1d1d278278322bfd71f1
MirjamFi <figasch@informatik.uni-tuebingen.de>
2019-10-23 08:25:38 +0200

    removed static mockup graphs for up/down stream information

--- static/scripts/server.js
+++ static/scripts/server.js
@@ -16,38 +16,6 @@ app.get('/about',function(req,res){
   res.sendFile(path.resolve('../../templates/about.html'));
 });
 
-app.get("/foundGraphs", function(req, res) {
-    if(foundFiles.length == 0){
-        var startPath = '../graphs/';
-        var filter = '.graphml';
-        fromDir(startPath, filter, foundFiles);
-    }   
-    res.send(foundFiles);}
-)
-
-
-function fromDir(startPath, filter){
-    if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
-        return;
-    }
-
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=path.join(startPath,files[i]);
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory()){
-            fromDir(filename,filter); //recurse
-        }
-        else if (filename.indexOf(filter)>=0) {
-            console.log('-- found: ',filename);
-            foundFiles.push(filename);
-        };
-    };
-    return foundFiles
-}
-
-
 app.listen(3000);
 
 
