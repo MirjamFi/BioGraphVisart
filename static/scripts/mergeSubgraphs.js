2aacfa85e35963b93562f832fd8ea529cbb328eb
MirjamFi <figasch@informatik.uni-tuebingen.de>
2019-11-29 16:43:44 +0100

    relative url for merged graph

--- static/scripts/mergeSubgraphs.js
+++ static/scripts/mergeSubgraphs.js
@@ -247,5 +247,5 @@ function merge(){
 function clickMerge(){
 	leftID = document.getElementById('leftID').innerHTML;
     rightID = document.getElementById('rightID').innerHTML;
-    window.open('http://localhost:3000/merge');
+    window.open('/merge');
 }
