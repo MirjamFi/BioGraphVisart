ed5764690052c75cef3f92bd97a0f2b7d1b96922
MirjamFi <figasch@informatik.uni-tuebingen.de>
2020-05-08 11:17:24 +0200

    Clean up

--- static/scripts/server.js
+++ static/scripts/server.js
@@ -32,49 +32,10 @@ app.get('/BioGraphVisart/contact',function(req,res){
   res.sendFile(path.resolve('../../templates/contact.html'));
 });
 
-app.get('/foundFilesInDirectory', function(req,res){
-    var startPath = '../../'+req.query.directory;
-    console.log(startPath)
-    if(startPath != prevdirectory){
-        foundFiles = [];
-        var filter = '.graphml';
-        var foundFilesInDirectory = fromDir(startPath, filter);
-        prevdirectory = startPath;
-        res.send(foundFilesInDirectory);
-    }
-})
-var mergedGraph;
-app.post('/BioGraphVisart/merge', function(req, res){
-	mergedGraph = req.body;
-	res.send(mergedGraph)
-})
 app.get('/BioGraphVisart/merge', function(req, res){
 	res.sendFile(path.resolve('../../templates/merge.html'));
 })
 
-function fromDir(startPath, filter){
-    if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
-        return;
-    }
-
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=path.join(startPath,files[i]);
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory()){
-            fromDir(filename,filter); //recurse
-        }
-        else if (filename.indexOf(filter)>=0) {
-            console.log('-- found: ',filename);
-            foundFiles.push(filename);
-        };
-    };
-    return foundFiles
-}
-
 app.listen(3000);
-
-
 
 console.log("Server running at http://localhost:3000/BioGraphVisart");
