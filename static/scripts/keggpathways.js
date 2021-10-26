a9d92b2fc1678540184e066fd049484a2681917e
MirjamFi <figasch@informatik.uni-tuebingen.de>
2021-10-26 15:40:17 +0200

    bug fix parsing entrez id for kegg pathways

--- static/scripts/keggpathways.js
+++ static/scripts/keggpathways.js
@@ -46,7 +46,7 @@ async function listKEGGPathways(ctx, cy, nodes, layer, canvas, pos = ""){
             var entrezID = n["data"]["entrez"].toString();            
           }
           else if(n["data"]["entrez_gene"] != undefined){
-            var splitentrez = n["data"]["entrez_gene"].split("/")
+            var splitentrez = n["data"]["entrez_gene"].toString().split("/")
             var entrezID = splitentrez[splitentrez.length-1].toString();            
           }
           let keggpaths = await getPathwaysFromKEGG(entrezID);
